d38e36fed053b1ec6ba5e2a3d80fb248d5ec8572
reynoldsjulia789 <jreynolds21@ewu.edu>
2026-04-30 13:02:04 -0700

    update

---
+++ src/test/AlignmentCheckerTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import main.InputFileReader;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AlignmentCheckerTest
+{
+    @Nested
+    @DisplayName("AlignmentTests")
+    class AlignmentTests
+    {
+        @Test
+        @DisplayName("test")
+        public void test()
+        {
+
+        }
+    }
+}
