5f29c8d8306bc32538e9b7251758eaed7458ce7a
reynoldsjulia789 <jreynolds21@ewu.edu>
2026-04-30 22:07:29 -0700

    update

--- src/main/AlignmentChecker.java
+++ src/main/AlignmentChecker.java
@@ -6,8 +6,9 @@ package main;
 public class AlignmentChecker
 {
     // modifiers are public so they can be accessed and changed from outside the class
-    public  int    matchBonus, mismatchPenalty, gapPenalty;
-    private String sequence1, sequence2;
+    public  int     matchBonus, mismatchPenalty, gapPenalty;
+    private int[][] editDistance;
+    private String  sequence1, sequence2;
 
     /**
      * Default AlignmentChecker constructor.
@@ -41,40 +42,117 @@ public class AlignmentChecker
      * Checks the alignment of two DNA sequences
      * @param sequence1 the first DNA sequence
      * @param sequence2 the second DNA sequence
+     * @throws IllegalArgumentException throws exception if one or both sequences are null or blank
      */
-    public String[] checkAlignment(String sequence1, String sequence2)
+    public String[] checkAlignment(String sequence1, String sequence2) throws IllegalArgumentException
     {
-        setSequences(sequence1, sequence2);
+        int sequence1Length, sequence2Length;
 
-        //TODO: finish checking alignment
+        if (sequence1 == null || sequence2 == null || sequence1.isBlank() || sequence2.isBlank())
+        {
+            throw new IllegalArgumentException("AlignmentChecker: can't align null sequence");
+        }
 
-        // return The score of the optimal alignment
-        //        The characters of the first string from the input file, with gaps ("-") added where necessary to
-        //            create the optimal alignment score.
-        //        The characters of the second string from the input file, with gaps ("-") added where necessary to
-        //            create the optimal alignment score.
-        return new String[] {null, null, null};
+        this.sequence1  = sequence1.trim();
+        this.sequence2  = sequence2.trim();
+
+        sequence1Length = this.sequence1.length();
+        sequence2Length = this.sequence2.length();
+
+        calculateEditDistances();
+        createAlignedStrings();
+
+        return new String[] {
+                                "Alignment Score: " + this.editDistance[sequence1Length][sequence2Length],
+                                this.sequence1,
+                                this.sequence2
+                            };
+    }
+
+    /** TODO: add a way to track where answer for each cell came from
+     * Fills out the 2D edit distance array.
+     */
+    private void calculateEditDistances()
+    {
+        int     seq1Idx, seq2Idx;
+        int     seq1SideLength, seq2SideLength;
+        int     matchMismatch, gapSeq1, gapSeq2;
+        int[][] editDistance;
+
+        seq1SideLength = this.sequence1.length() + 1;
+        seq2SideLength = this.sequence2.length() + 1;
+
+        editDistance = new int[seq1SideLength][seq2SideLength];
+
+        // fill in the edit distances for the 1st column and 1st row
+        for (seq1Idx = 1; seq1Idx < seq1SideLength; seq1Idx++)
+        {
+            editDistance[0][seq1Idx] = seq1Idx * this.gapPenalty;
+        }
+
+        for (seq2Idx = 1; seq2Idx < seq2SideLength; seq2Idx++)
+        {
+            editDistance[seq2Idx][0] = seq2Idx * this.gapPenalty;
+        }
+
+        // fill in the rest of the edit distances
+        for (seq1Idx = 1; seq1Idx < seq1SideLength; seq1Idx++)
+        {
+            for (seq2Idx = 1; seq2Idx < seq2SideLength; seq2Idx++)
+            {
+                matchMismatch = cost(
+                                        this.sequence1.charAt(seq1Idx - 1),
+                                        this.sequence2.charAt(seq2Idx - 1),
+                                        editDistance[seq2Idx - 1][seq1Idx - 1]
+                                    );
+                gapSeq1       = this.gapPenalty + editDistance[seq2Idx][seq1Idx - 1];
+                gapSeq2       = this.gapPenalty + editDistance[seq2Idx - 1][seq1Idx];
+
+                if (matchMismatch >= gapSeq1 && matchMismatch >= gapSeq2)
+                {
+                    editDistance[seq2Idx][seq1Idx] = matchMismatch;
+                    // TODO: store that we are going with match/mismatch
+                }
+                else if (gapSeq1 > matchMismatch && gapSeq1 >= gapSeq2)
+                {
+                    editDistance[seq2Idx][seq1Idx] = gapSeq1;
+                    // TODO: store that we are going with gapSeq1
+                }
+                else
+                {
+                    editDistance[seq2Idx][seq1Idx] = gapSeq2;
+                    // TODO: store that we are going with gapSeq2
+                }
+            }
+        }
+
+        this.editDistance = editDistance;
     }
 
     /**
-     * Sets the two DNA sequences to be checked
-     * @param sequence1 the first DNA sequence
-     * @param sequence2 the second DNA sequence
+     * Determines if two chars match or not and calculates the associated cost
+     * @param char1 the first character to compare
+     * @param char2 the second character to compare
+     * @return returns the cost of considering a match/mutation between the two characters
      */
-    private void setSequences(String sequence1, String sequence2)
+    private int cost(char char1, char char2, int total)
     {
-        if (sequence1 == null)
+        if (char1 == char2)
         {
-            throw new IllegalArgumentException("AlignmentChecker: can't set null sequence");
+            return total + this.matchBonus;
         }
 
-        if (sequence2 == null)
-        {
-            throw new IllegalArgumentException("AlignmentChecker: can't set null sequence");
-        }
+        return total + this.mismatchPenalty;
+    }
 
-        this.sequence1 = sequence1.trim();
-        this.sequence2 = sequence2.trim();
+    /** TODO: finish createAlignedStrings
+     * Takes a calculated edit distance 2D array and the original sequence and adds gaps where necessary to create
+     * the optimal alignment score.
+     */
+    private void createAlignedStrings()
+    {
+        // save the characters of the strings with gaps ("-") added where necessary to create the optimal alignment score
+
     }
 
     @Override
