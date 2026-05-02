b5795d34182cf3f2c3efa524db1323ec90b98fce
reynoldsjulia789 <jreynolds21@ewu.edu>
2026-05-01 20:04:04 -0700

    update

--- src/test/AlignmentCheckerTest.java
+++ src/test/AlignmentCheckerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AlignmentCheckerTest
 {
@@ -15,7 +16,7 @@ public class AlignmentCheckerTest
     {
         @Test
         @DisplayName("finds best alignment of AGCGA and ACGAA")
-        public void test1()
+        public void testMatchGap()
         {
             AlignmentChecker         test;
             AlignmentChecker.Results results;
@@ -30,7 +31,7 @@ public class AlignmentCheckerTest
 
         @Test
         @DisplayName("finds best alignment of AGTTA and AGAGA")
-        public void test2()
+        public void testMatchMismatch()
         {
             AlignmentChecker         test;
             AlignmentChecker.Results results;
@@ -44,8 +45,8 @@ public class AlignmentCheckerTest
         }
 
         @Test
-        @DisplayName("finds best alignment of ACGT and GT")
-        public void test3()
+        @DisplayName("finds best alignment of ACGT and GT (trailing match)")
+        public void testTrialing()
         {
             AlignmentChecker         test;
             AlignmentChecker.Results results;
@@ -56,6 +57,12 @@ public class AlignmentCheckerTest
             assertEquals(-2, results.alignmentScore(), "Alignment Score");
             assertEquals("ACGT", results.sequence1(), "Sequence 1");
             assertEquals("--GT", results.sequence2(), "Sequence 2");
+
+            results = test.checkAlignment("GT", "ACGT");
+
+            assertEquals(-2, results.alignmentScore(), "Alignment Score");
+            assertEquals("--GT", results.sequence1(), "Sequence 1");
+            assertEquals("ACGT", results.sequence2(), "Sequence 2");
         }
 
         @Test
@@ -71,6 +78,106 @@ public class AlignmentCheckerTest
             assertEquals(22, results.alignmentScore(), "Alignment Score");
             assertEquals("GTGTGTGTGGGTGCACATTTGTGTGTGTGTGCGCCTGTGTGTGTGGGTGCCTGTGTGTGT", results.sequence1(), "Sequence 1");
             assertEquals("GTGTGTGTGGAAGTGAGTTCATCTGTGTGTGC-AC-ATGTGTGTGCATGCATGCATGTGT", results.sequence2(), "Sequence 2");
+        }
+
+        @Test
+        @DisplayName("finds best alignment of ACGT and AC (leading match)")
+        public void testleadingmatch()
+        {
+            AlignmentChecker         test;
+            AlignmentChecker.Results results;
+
+            test    = new AlignmentChecker();
+            results = test.checkAlignment("ACGT", "AC");
+
+            assertEquals(-2, results.alignmentScore(), "Alignment Score");
+            assertEquals("ACGT", results.sequence1(), "Sequence 1");
+            assertEquals("AC--", results.sequence2(), "Sequence 2");
+
+            results = test.checkAlignment("AC", "ACGT");
+
+            assertEquals(-2, results.alignmentScore(), "Alignment Score");
+            assertEquals("AC--", results.sequence1(), "Sequence 1");
+            assertEquals("ACGT", results.sequence2(), "Sequence 2");
+        }
+
+        @Test
+        @DisplayName("finds best alignment of AT and CG (no match)")
+        public void testnomatch()
+        {
+            AlignmentChecker         test;
+            AlignmentChecker.Results results;
+
+            test    = new AlignmentChecker();
+            results = test.checkAlignment("AT", "CG");
+
+            assertEquals(-2, results.alignmentScore(), "Alignment Score");
+            assertEquals("AT", results.sequence1(), "Sequence 1");
+            assertEquals("CG", results.sequence2(), "Sequence 2");
+        }
+
+        @Test
+        @DisplayName("finds best alignment of ACGT and CG (middle match)")
+        public void testmiddlematch()
+        {
+            AlignmentChecker         test;
+            AlignmentChecker.Results results;
+
+            test    = new AlignmentChecker();
+            results = test.checkAlignment("ACGT", "CG");
+
+            assertEquals(-2, results.alignmentScore(), "Alignment Score");
+            assertEquals("ACGT", results.sequence1(), "Sequence 1");
+            assertEquals("-CG-", results.sequence2(), "Sequence 2");
+
+            results = test.checkAlignment("CG", "ACGT");
+
+            assertEquals(-2, results.alignmentScore(), "Alignment Score");
+            assertEquals("-CG-", results.sequence1(), "Sequence 1");
+            assertEquals("ACGT", results.sequence2(), "Sequence 2");
+        }
+
+        @Test
+        @DisplayName("finds best alignment of GAATTC & GATTA")
+        public void testMatchMismatchGap()
+        {
+            AlignmentChecker         test;
+            AlignmentChecker.Results results;
+
+            test    = new AlignmentChecker();
+            results = test.checkAlignment("GAATTC", "GATTA");
+
+            assertEquals("GAATTC", results.sequence1(), "Sequence 1");
+            assertEquals("G-ATTA", results.sequence2(), "Sequence 2");
+        }
+
+        @Test
+        @DisplayName("throws exception on empty sequence")
+        public void testEmpty()
+        {
+            AlignmentChecker         test;
+
+            test    = new AlignmentChecker();
+
+            assertThrows(Exception.class, () -> test.checkAlignment("ACGT", " "));
+            assertThrows(Exception.class, () -> test.checkAlignment(" ", "ACGT"));
+        }
+
+        @Test
+        @DisplayName("trims spaces from the outer edges of sequences")
+        public void testSpace()
+        {
+            AlignmentChecker         test;
+            AlignmentChecker.Results results;
+
+            test    = new AlignmentChecker();
+            results = test.checkAlignment("ACGT", " ACGT ");
+
+            assertEquals("ACGT", results.sequence2(), "Sequence 2");
+
+            results = test.checkAlignment(" ACGT ", "ACGT");
+
+            assertEquals("ACGT", results.sequence1(), "Sequence 1");
         }
     }
 }
